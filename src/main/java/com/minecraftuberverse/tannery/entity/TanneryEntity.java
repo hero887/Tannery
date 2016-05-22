1b3a92b4b5a78fb91b6c0cf5fa0f2fa7ea375fb9
SoggyMustache <soggymustacheofficialmods@gmail.com>
2016-05-22 15:04:40 -0400

    Entity files
    I only added the elk which is basically a cow for now.

---
+++ src/main/java/com/minecraftuberverse/tannery/entity/TanneryEntity.java
@@ -0,0 +1,34 @@
+package com.minecraftuberverse.tannery.entity;
+
+
+import com.minecraftuberverse.tannery.Tannery;
+import com.minecraftuberverse.tannery.entity.elk.Elk;
+
+import net.minecraft.entity.EntityList;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+
+//this is a basic Entity class
+
+public class TanneryEntity {
+	public static void Tannery(){
+		registerEntity();
+	}
+	public static void registerEntity(){
+		//Example
+		//createEntity(Kangaroo.class, "Kangaroo", 0xDEA02C, 0xE3C286);
+		createEntity(Elk.class, "Elk", 0xFFFFFF, 0xFFFFFFF);
+
+		
+		
+	}
+	public static void createEntity(Class entityClass, String entityName, int solidColor, int spotColor){
+		int randomId = EntityRegistry.findGlobalUniqueEntityId();
+		
+		EntityRegistry.registerGlobalEntityID(entityClass, entityName, randomId);
+		EntityRegistry.registerModEntity(entityClass, entityName, randomId, Tannery.modInstance, 42, 1, true);
+		createEgg(randomId, solidColor, spotColor);
+	}
+	private static void createEgg(int randomId, int solidColor, int spotColor) {
+		EntityList.entityEggs.put(Integer.valueOf(randomId), new EntityList.EntityEggInfo(randomId, solidColor, spotColor));
+	}
+}
---
+++ src/main/java/com/minecraftuberverse/tannery/entity/elk/Elk.java
@@ -0,0 +1,138 @@
+package com.minecraftuberverse.tannery.entity.elk;
+
+import net.minecraft.block.Block;
+import net.minecraft.entity.EntityAgeable;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIEatGrass;
+import net.minecraft.entity.ai.EntityAIFollowParent;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAIMate;
+import net.minecraft.entity.ai.EntityAIMoveToBlock;
+import net.minecraft.entity.ai.EntityAIPanic;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAITempt;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.passive.EntityAnimal;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.pathfinding.PathNavigateGround;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.World;
+
+public class Elk extends EntityAnimal
+{
+
+    public Elk(World worldIn)
+    {
+        super(worldIn);
+        this.setSize(0.9F, 1.3F);
+        ((PathNavigateGround)this.getNavigator()).func_179690_a(true);
+        this.tasks.addTask(0, new EntityAISwimming(this));
+        this.tasks.addTask(1, new EntityAIPanic(this, 2.0D));
+        this.tasks.addTask(2, new EntityAIMate(this, 1.0D));
+        this.tasks.addTask(3, new EntityAITempt(this, 1.25D, Items.wheat, false));
+        this.tasks.addTask(4, new EntityAIFollowParent(this, 1.25D));
+        this.tasks.addTask(5, new EntityAIWander(this, 1.0D));
+        this.tasks.addTask(5, new EntityAIEatGrass(this));
+        this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
+        this.tasks.addTask(7, new EntityAILookIdle(this));
+
+    }
+
+    protected void applyEntityAttributes()
+    {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(10.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.20000000298023224D);
+    }
+
+    protected String getLivingSound()
+    {
+        return "tannery:mob.elk.say";
+    }
+
+    protected String getHurtSound()
+    {
+        return "tannery:mob.elk.hurt";
+    }
+    protected String getDeathSound()
+    {
+        return "tannery:mob.elk.hurt";
+    }
+
+    protected void playStepSound(BlockPos p_180429_1_, Block p_180429_2_)
+    {
+        this.playSound("tannery:mob.elk.step", 0.15F, 1.0F);
+    }
+
+    protected float getSoundVolume()
+    {
+        return 0.4F;
+    }
+
+    protected Item getDropItem()
+    {
+        return Items.leather;
+    }
+    protected void dropFewItems(boolean p_70628_1_, int p_70628_2_)
+    {
+        int j = this.rand.nextInt(3) + this.rand.nextInt(1 + p_70628_2_);
+        int k;
+
+        for (k = 0; k < j; ++k)
+        {
+            this.dropItem(Items.leather, 1);
+        }
+
+        j = this.rand.nextInt(3) + 1 + this.rand.nextInt(1 + p_70628_2_);
+
+        for (k = 0; k < j; ++k)
+        {
+            if (this.isBurning())
+            {
+                this.dropItem(Items.cooked_beef, 1);
+            }
+            else
+            {
+                this.dropItem(Items.beef, 1);
+            }
+        }
+    }
+
+    public boolean interact(EntityPlayer player)
+    {
+        ItemStack itemstack = player.inventory.getCurrentItem();
+
+        if (itemstack != null && itemstack.getItem() == Items.bucket && !player.capabilities.isCreativeMode)
+        {
+            if (itemstack.stackSize-- == 1)
+            {
+                player.inventory.setInventorySlotContents(player.inventory.currentItem, new ItemStack(Items.milk_bucket));
+            }
+            else if (!player.inventory.addItemStackToInventory(new ItemStack(Items.milk_bucket)))
+            {
+                player.dropPlayerItemWithRandomChoice(new ItemStack(Items.milk_bucket, 1, 0), false);
+            }
+
+            return true;
+        }
+        else
+        {
+            return super.interact(player);
+        }
+    }
+
+    public Elk createChild(EntityAgeable ageable)
+    {
+        return new Elk(this.worldObj);
+    }
+
+    public float getEyeHeight()
+    {
+        return this.height;
+    }
+}
