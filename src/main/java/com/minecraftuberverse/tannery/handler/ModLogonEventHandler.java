80678fa1b00477f2505a106aac72c1a790bfca15
SoggyMustache <soggymustacheofficialmods@gmail.com>
2016-05-22 13:20:32 -0400

    Update ModLogonEventHandler.java

--- src/main/java/com/minecraftuberverse/tannery/handler/ModLogonEventHandler.java
+++ src/main/java/com/minecraftuberverse/tannery/handler/ModLogonEventHandler.java
@@ -28,35 +28,7 @@ public class ModLogonEventHandler
 							"Made By SoggyMustache, Hero887, LewisMcReu, BubbleTrouble, TheJurrasicAlien, Wolfgank, RdV01")));
 			player.addChatMessage(prefix);
 
-			ChatComponentText url;
-			Random rand = new Random();
-			int sel = rand.nextInt(3);
-			switch (sel)
-			{
-				case 1:
-					player.addChatMessage(new ChatComponentText(
-							EnumChatFormatting.GOLD + PREFIX + EnumChatFormatting.GREEN + "Make sure you get all the new updates at soggymustache.tk"));
-					url = new ChatComponentText(
-							EnumChatFormatting.GOLD + PREFIX + EnumChatFormatting.RESET + "soggymustache.tk");
-					url.getChatStyle().setChatClickEvent(
-							new ClickEvent(ClickEvent.Action.OPEN_URL, "http://soggymustache.tk"));
-					url.getChatStyle().setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-							new ChatComponentText("Open URL")));
-					player.addChatMessage(url);
-					break;
-				case 2:
-					player.addChatMessage(new ChatComponentText(
-							EnumChatFormatting.GOLD + PREFIX + EnumChatFormatting.GREEN + "Check out SoggyMustache's website for more mods"));
-					url = new ChatComponentText(
-							EnumChatFormatting.GOLD + PREFIX + EnumChatFormatting.RESET + "soggymustache.tk");
-					url.getChatStyle().setChatClickEvent(
-							new ClickEvent(ClickEvent.Action.OPEN_URL, "http://soggymustache.tk"));
-					url.getChatStyle().setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-							new ChatComponentText("Open URL")));
-					player.addChatMessage(url);
-					break;
-			}
-			// ConfigurationHandler.hasDisplayedOnce = true;
-		}
+
 	}
+}
 }
