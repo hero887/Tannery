9d8238765293b812838d011cc424fa093b2affe8
SoggyMustache <soggymustacheofficialmods@gmail.com>
2016-05-23 17:08:06 -0400

    Update ClientProxy.java

--- src/main/java/com/minecraftuberverse/tannery/proxy/ClientProxy.java
+++ src/main/java/com/minecraftuberverse/tannery/proxy/ClientProxy.java
@@ -1,7 +1,13 @@
 package com.minecraftuberverse.tannery.proxy;
 
+import com.minecraftuberverse.tannery.entity.elk.Elk;
+import com.minecraftuberverse.tannery.entity.elk.ModelElk;
+import com.minecraftuberverse.tannery.entity.elk.RenderElk;
 import com.minecraftuberverse.tannery.init.TanneryBlocks;
 import com.minecraftuberverse.tannery.init.TanneryItems;
+
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 
 public class ClientProxy extends CommonProxy
 {
@@ -16,5 +22,6 @@ public class ClientProxy extends CommonProxy
 
 		TanneryItems.registerRenderers();
 		TanneryBlocks.registerRenderers();
+		RenderingRegistry.registerEntityRenderingHandler(Elk.class, new RenderElk(Minecraft.getMinecraft().getRenderManager(), new ModelElk(), 0));
 	}
 }
