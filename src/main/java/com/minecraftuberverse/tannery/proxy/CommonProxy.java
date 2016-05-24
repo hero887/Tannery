16c3364284351421c476ea27d3f3cd41ca0d22bc
SoggyMustache <soggymustacheofficialmods@gmail.com>
2016-05-24 18:04:24 -0400

    Update CommonProxy.java
    All wool can be crafted into string

--- src/main/java/com/minecraftuberverse/tannery/proxy/CommonProxy.java
+++ src/main/java/com/minecraftuberverse/tannery/proxy/CommonProxy.java
@@ -27,8 +27,22 @@ public abstract class CommonProxy
 
 	public void init()
 	{
-		GameRegistry.addShapelessRecipe(new ItemStack(Items.string, 4),
-				new Object[] { Item.getItemFromBlock(Blocks.wool) });
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.string, 4), new Object[] {new ItemStack(Blocks.wool, 1, 0)});
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.string, 4), new Object[] {new ItemStack(Blocks.wool, 1, 1)});
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.string, 4), new Object[] {new ItemStack(Blocks.wool, 1, 2)});
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.string, 4), new Object[] {new ItemStack(Blocks.wool, 1, 3)});
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.string, 4), new Object[] {new ItemStack(Blocks.wool, 1, 4)});
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.string, 4), new Object[] {new ItemStack(Blocks.wool, 1, 5)});
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.string, 4), new Object[] {new ItemStack(Blocks.wool, 1, 6)});
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.string, 4), new Object[] {new ItemStack(Blocks.wool, 1, 7)});
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.string, 4), new Object[] {new ItemStack(Blocks.wool, 1, 8)});
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.string, 4), new Object[] {new ItemStack(Blocks.wool, 1, 9)});
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.string, 4), new Object[] {new ItemStack(Blocks.wool, 1, 10)});
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.string, 4), new Object[] {new ItemStack(Blocks.wool, 1, 11)});
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.string, 4), new Object[] {new ItemStack(Blocks.wool, 1, 12)});
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.string, 4), new Object[] {new ItemStack(Blocks.wool, 1, 13)});
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.string, 4), new Object[] {new ItemStack(Blocks.wool, 1, 14)});
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.string, 4), new Object[] {new ItemStack(Blocks.wool, 1, 15)});
 	}
 
 	public void postInit()
